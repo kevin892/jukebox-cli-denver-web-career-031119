fd40805532ec7fa862f8341a948bcac388bf679b
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-02-07 19:37:45 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,79 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
+end
+
+def list(songs)
+  puts "
+1. Phoenix - 1901
+2. Tokyo Police Club - Wait Up
+3. Sufjan Stevens - Too Much
+4. The Naked and the Famous - Young Blood
+5. (Far From) Home - Tiga
+6. The Cults - Abducted
+7. Phoenix - Consolation Prizes
+8. Harry Chapin - Cats in the Cradle
+9. Amos Lee - Keep It Loose, Keep It Tight"
+end
+
+def play(songs)
+  puts "Please enter a song name or number: "
+  user_input = gets.chomp
+  songs.each_with_index do |song, value|
+    if song == user_input || song == songs[(user_input.to_i)-1]
+        puts "Playing #{song}"
+      else
+        puts "Invalid input, please try again"
+      end
+    end
+end
+
+# def exit_jukebox
+#   puts "Goodbye"
+# end
+#
+# def run
+#   help
+#   puts "Please enter a command: "
+#   user_command = gets.chomp
+#   while user_command != "exit"
+# case user_command
+# when "list" then list(songs)
+# when "play" then play(songs)
+# when "help" then help
+# when "exit" then exit
+# else "Invalid entry"
+#     end
+#   end
+# end
+def run
+  help
+  puts "Please enter a command:"
+  user_input = gets.chomp
+  until user_input == "exit"
+    if user_input == "help"
+      help
+      puts "Please enter a command:"
+      user_input = gets.chomp
+    elsif user_input == "list"
+      list(songs)
+      puts "Please enter a command:"
+      user_input = gets.chomp
+    elsif user_input == "play"
+      play(songs)
+      puts "Please enter a command:"
+      user_input = gets.chomp
+    elsif user_input == "exit"
+      return exit_jukebox
+    else
+      "Invalid entry"
+    end
+  end
+   exit_jukebox
+end
